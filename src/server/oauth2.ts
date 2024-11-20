dad3339e0acaf2355dd6f609aa6a62a0e74f98cc
unusualjon <67762832+unusualjon@users.noreply.github.com>
2024-11-20 09:45:28 -0500

    fix typos

--- src/server/oauth2.ts
+++ src/server/oauth2.ts
@@ -1,8 +1,8 @@
 import { google } from 'googleapis';
-import { AUTH_CIENT_ID, AUTH_CIENT_SECRET } from '$env/static/private';
+import { AUTH_CLIENT_ID, AUTH_CLIENT_SECRET } from '$env/static/private';
 
 export const oauth2Client = new google.auth.OAuth2(
-	AUTH_CIENT_ID,
-	AUTH_CIENT_SECRET,
+	AUTH_CLIENT_ID,
+	AUTH_CLIENT_SECRET,
 	'http://localhost:5173/api/auth/callback'
 );
